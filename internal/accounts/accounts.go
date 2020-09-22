2bac86bb332f0954feffcbdb28f7e7468672f47f
Amin Talebi <amin.talebi@snapp.cab>
2020-09-22 15:31:36 +0330

    clean master

--- internal/accounts/accounts.go
+++
@@ -1,8 +0,0 @@
-package accounts
-
-import "gitlab.snapp.ir/dispatching/soteria/internal/db"
-
-// Service is responsible for all things related to account handling
-type Service struct {
-	Handler db.ModelHandler
-}
