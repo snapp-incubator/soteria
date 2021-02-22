9e501a00166e60ef95cf243461b3766d00fdb73e
Narges Ahmadi <narges.ahmadi@snapp.cab>
2021-02-22 15:38:51 +0330

    Fix jwt rsync

--- .gitlab/ci/scripts/update-environments.sh
+++ .gitlab/ci/scripts/update-environments.sh
@@ -83,7 +83,7 @@ echo "${ENV_DRIVER_JWT_PUBLIC_KEY_PRODUCTION}" > "jwt"/0.pem
 echo "${ENV_PASSENGER_JWT_PUBLIC_KEY_PRODUCTION}" > "jwt"/1.pem
 echo "${ENV_THIRD_PARTY_JWT_PUBLIC_KEY_PRODUCTION}" > "jwt"/100.pem
 echo "${ENV_THIRD_PARTY_JWT_PRIVATE_KEY_PRODUCTION}" > "jwt"/100.private.pem
-rsync -e 'ssh -o "StrictHostKeyChecking=no"' -avzr ./jwt "$APP_USERNAME@$APP_HOSTNAME:${ENV_SOTERIA_JWT_KEYS_PATH}"
+rsync -e 'ssh -o "StrictHostKeyChecking=no"' -avz ./jwt/* "$APP_USERNAME@$APP_HOSTNAME:${ENV_SOTERIA_JWT_KEYS_PATH}"
 rm -r jwt 
 
 ### Green service config file
