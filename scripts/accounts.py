80bfab041538d9864bfd13a756651d22542a1b1e
Parham Alvani <parham.alvani@gmail.com>
2021-04-04 22:34:24 +0430

    feat: Update Scripts

--- scripts/accounts.py
+++ scripts/accounts.py
@@ -1,21 +1,33 @@
 import requests
-import json
 import base64
+import pprint
 
 
-base_url = 'https://soteria-snapp-ode-012.apps.private.teh-1.snappcloud.io/'
+base_url = "https://soteria-snapp-ode-012.apps.private.teh-1.snappcloud.io/"
+pp = pprint.PrettyPrinter(indent=4)
 
 
 def get_account(username, password):
-    url = base_url + 'accounts/' + username
-    auth_bytes = '{}:{}'.format(username, password).encode('ascii')
-    auth = base64.b64encode(auth_bytes).decode('ascii')
-    
-    res = requests.get(url, headers={
-        "Authorization": "Basic " + auth
-    })
-    return json.dumps(res.json(), indent=4, sort_keys=True)
+
+    url = base_url + "accounts/" + username
+
+    res = requests.get(url, auth=(username, password))
+    return pp.pprint(res.json())
 
 
+print(get_account("driver", "password"))
+print(get_account("daghigh", "secret"))
 
-print(get_account('driver', 'password'))
+
+def update_account(username, password, secret):
+    url = base_url + "accounts/" + username
+
+    body = {
+        secret: "secret",
+    }
+
+    res = requests.put(url, auth=(username, password), json=body)
+    return pp.pprint(res.json())
+
+
+print(update_account("daghigh", "secret", "secret"))
--- scripts/get_token.py
+++ scripts/get_token.py
@@ -2,18 +2,21 @@ import requests
 
 from vars import *
 
-base_url = 'https://soteria-snapp-ode-012.apps.private.teh-1.snappcloud.io/'
-token_url = base_url + 'token'
+base_url = "https://soteria-snapp-ode-012.apps.private.teh-1.snappcloud.io/"
+token_url = base_url + "token"
 
 
 def get_token(grant_type, client_id, client_secret):
-    res = requests.post(token_url, data={
-        'grant_type': grant_type,
-        'client_id': client_id,
-        'client_secret': client_secret
-        })
-    return res.content.decode('utf-8')
+    res = requests.post(
+        token_url,
+        data={
+            "grant_type": grant_type,
+            "client_id": client_id,
+            "client_secret": client_secret,
+        },
+    )
+    return res.content.decode("utf-8")
 
-print(get_token(SUBSCRIE, 'box', 'secret'))
 
-
+print(get_token(SUBSCRIE, "box", "secret"))
+print(get_token(SUBSCRIE, "daghigh", "secret"))
