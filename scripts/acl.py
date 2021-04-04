6ae5185acfd502eb181cffad515ac0af9e120fa7
Amin Talebi <amin.talebi@snapp.cab>
2021-04-04 17:24:38 +0430

    add get token endpoint

--- scripts/acl.py
+++ scripts/acl.py
@@ -3,10 +3,10 @@ import requests
 from vars import *
 
 base_url = 'https://soteria-snapp-ode-012.apps.private.teh-1.snappcloud.io/'
-auth_url = base_url + 'acl'
+acl_url = base_url + 'acl'
 
 
-res = requests.post(auth_url, data={
+res = requests.post(acl_url, data={
     'access': PUBLISH,
     'token': DRIVER_TOKEN,
     'topic': 'snapp/driver/1234/location'
