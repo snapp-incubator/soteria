4deb2a7adfaaeae8705b4f23a64bf04abb8c7c55
Amin Talebi <amin@Amins-MacBook-Pro.local>
2021-04-04 17:11:18 +0430

    add auth, acl, get account script

---
+++ scripts/auth.py
@@ -0,0 +1,13 @@
+import requests 
+
+from vars import *
+
+base_url = 'https://soteria-snapp-ode-012.apps.private.teh-1.snappcloud.io/'
+auth_url = base_url + 'auth'
+
+
+res = requests.post(auth_url, data={
+    'token': DRIVER_TOKEN,
+    })
+
+print(res.content)
