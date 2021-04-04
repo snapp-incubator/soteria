265260a79b01771336f48332fb6660cbd8b74c04
Parham Alvani <parham.alvani@gmail.com>
2021-04-04 22:34:24 +0430

    feat: Update Scripts

--- scripts/auth.py
+++ scripts/auth.py
@@ -1,13 +1,16 @@
-import requests 
+import requests
 
-from vars import *
+from vars import DRIVER_TOKEN
 
-base_url = 'https://soteria-snapp-ode-012.apps.private.teh-1.snappcloud.io/'
-auth_url = base_url + 'auth'
+base_url = "https://soteria-snapp-ode-012.apps.private.teh-1.snappcloud.io"
+auth_url = base_url + "/auth"
 
 
-res = requests.post(auth_url, data={
-    'token': DRIVER_TOKEN,
-    })
+res = requests.post(
+    auth_url,
+    data={
+        "token": DRIVER_TOKEN,
+    },
+)
 
 print(res.content)
