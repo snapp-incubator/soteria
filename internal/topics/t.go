3bf1e53384f2fa92a6eea3e87dde1b70be5f620c
mohammadreza.modares <mohammadreza.modares@snapp.cab>
2021-05-17 16:37:17 +0430

    Add gossiper and passenger location topics

--- internal/topics/t.go
+++ internal/topics/t.go
@@ -7,20 +7,22 @@ type Topic string
 type Type string
 
 const (
-	CabEvent         Type = "cab_event"
-	DriverLocation   Type = "driver_location"
-	SuperappEvent    Type = "superapp_event"
-	BoxEvent         Type = "box_event"
-	DaghighSys       Type = "daghigh_sys"
-	GossiperLocation Type = "gossiper_location"
+	CabEvent          Type = "cab_event"
+	DriverLocation    Type = "driver_location"
+	PassengerLocation Type = "passenger_location"
+	SuperappEvent     Type = "superapp_event"
+	BoxEvent          Type = "box_event"
+	DaghighSys        Type = "daghigh_sys"
+	GossiperLocation  Type = "gossiper_location"
 )
 
 var (
-	CabEventRegexp         = regexp.MustCompile(`(\w+)-event-[a-zA-Z0-9]+`)
-	DriverLocationRegexp   = regexp.MustCompile(`snapp/driver/[a-zA-Z0-9+]+/location`)
-	SuperappEventRegexp    = regexp.MustCompile(`snapp/(\w+)/[a-zA-Z0-9]+/superapp`)
-	GossiperLocationRegexp = regexp.MustCompile(`snapp/passenger/[a-zA-Z0-9]+/driver-location`)
-	DaghighSysRegexp       = regexp.MustCompile(`\$SYS/brokers/\+/clients/\+/(connected|disconnected)`)
+	CabEventRegexp          = regexp.MustCompile(`(\w+)-event-[a-zA-Z0-9]+`)
+	DriverLocationRegexp    = regexp.MustCompile(`snapp/driver/[a-zA-Z0-9+]+/location`)
+	PassengerLocationRegexp = regexp.MustCompile(`snapp/passenger/[a-zA-Z0-9+]+/location`)
+	SuperappEventRegexp     = regexp.MustCompile(`snapp/driver|passenger/[a-zA-Z0-9]+/(superapp)`)
+	GossiperLocationRegexp  = regexp.MustCompile(`snapp+/(driver|passenger)+/[a-zA-Z0-9]+/(driver-location|passenger-location)`)
+	DaghighSysRegexp        = regexp.MustCompile(`\$SYS/brokers/\+/clients/\+/(connected|disconnected)`)
 )
 
 func (t Topic) GetType() Type {
@@ -31,12 +33,14 @@ func (t Topic) GetType() Type {
 		return CabEvent
 	case DriverLocationRegexp.MatchString(topic):
 		return DriverLocation
+	case PassengerLocationRegexp.MatchString(topic):
+		return PassengerLocation
+	case GossiperLocationRegexp.MatchString(topic):
+		return GossiperLocation
 	case SuperappEventRegexp.MatchString(topic):
 		return SuperappEvent
 	case topic == "bucks":
 		return BoxEvent
-	case GossiperLocationRegexp.MatchString(topic):
-		return GossiperLocation
 	case DaghighSysRegexp.MatchString(topic):
 		return DaghighSys
 	}
