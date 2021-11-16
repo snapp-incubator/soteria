786e292675a5eef0ecb60f49171d607f3af53916
Parham Alvani <parham.alvani@gmail.com>
2021-11-16 23:55:55 +0330

    feat: remove redis databases

--- internal/db/models.go
+++ internal/db/models.go
@@ -1,28 +1,9 @@
 package db
 
 import (
-	"context"
-	"errors"
-	"time"
+	"gitlab.snapp.ir/dispatching/soteria/v3/pkg/user"
 )
 
-// ErrDb is returned when there was an error in database operations
-var ErrDb = errors.New("database error")
-
 type ModelHandler interface {
-	Save(ctx context.Context, model Model) error
-	Delete(ctx context.Context, modelName, pk string) error
-	Get(ctx context.Context, modelName, pk string, model Model) error
-	Update(ctx context.Context, model Model) error
-}
-
-type Model interface {
-	GetMetadata() MetaData
-	GetPrimaryKey() string
-}
-
-type MetaData struct {
-	ModelName    string    `json:"model_name"`
-	DateCreated  time.Time `json:"date_created"`
-	DateModified time.Time `json:"date_modified"`
+	Get(pk string) user.User
 }
