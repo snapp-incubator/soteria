d0843c1a365e4aef8097992dba33e2c4b782b7d2
Parham Alvani <parham.alvani@gmail.com>
2021-11-16 23:59:28 +0330

    feat: remove other commands

--- cmd/soteria/soteria.go
+++ cmd/soteria/soteria.go
@@ -4,17 +4,12 @@ import (
 	"log"
 
 	"gitlab.snapp.ir/dispatching/soteria/v3/internal/commands"
-	"gitlab.snapp.ir/dispatching/soteria/v3/internal/commands/accounts"
 )
 
 func main() {
 	cli := commands.Root
 
 	cli.AddCommand(commands.Serve)
-
-	accounts.Accounts.AddCommand(accounts.Init)
-	cli.AddCommand(accounts.Accounts)
-	cli.AddCommand(commands.Token)
 
 	if err := cli.Execute(); err != nil {
 		log.Fatal(err)
