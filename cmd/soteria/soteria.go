e6c5282695d7f836144d203a603e6c4cc25cded5
Parham Alvani <parham.alvani@gmail.com>
2021-11-16 20:22:27 +0330

    feat: remove init function

--- cmd/soteria/soteria.go
+++ cmd/soteria/soteria.go
@@ -7,17 +7,15 @@ import (
 	"gitlab.snapp.ir/dispatching/soteria/v3/internal/commands/accounts"
 )
 
-var cli = commands.Root
+func main() {
+	cli := commands.Root
 
-func init() {
 	cli.AddCommand(commands.Serve)
 
 	accounts.Accounts.AddCommand(accounts.Init)
 	cli.AddCommand(accounts.Accounts)
 	cli.AddCommand(commands.Token)
-}
 
-func main() {
 	if err := cli.Execute(); err != nil {
 		log.Fatal(err)
 	}
