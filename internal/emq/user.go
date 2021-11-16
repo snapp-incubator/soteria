90e8a122ea02779b8af0ea3fe468050950a8a876
Parham Alvani <parham.alvani@gmail.com>
2021-11-16 21:39:27 +0330

    feat: remove emq package

--- internal/emq/user.go
+++
@@ -1,9 +0,0 @@
-package emq
-
-// User contains the user information for storing in emq based on its redis-auth plugin.
-// https://github.com/emqx/emqx-auth-redis
-type User struct {
-	Username    string
-	Password    string
-	IsSuperuser bool
-}
