deeeba364d76fdd0089367aab781bac9c6289222
Parham Alvani <parham.alvani@gmail.com>
2021-11-17 00:00:27 +0330

    feat: complete without redis

--- internal/app/a.go
+++ internal/app/a.go
@@ -5,51 +5,40 @@ import (
 	"sync"
 
 	"github.com/opentracing/opentracing-go"
-	"gitlab.snapp.ir/dispatching/soteria/v3/internal/accounts"
 	"gitlab.snapp.ir/dispatching/soteria/v3/internal/authenticator"
-	"gitlab.snapp.ir/dispatching/soteria/v3/internal/emq"
 	"gitlab.snapp.ir/dispatching/soteria/v3/pkg/metrics"
 )
 
-type app struct {
-	Metrics         metrics.Metrics
-	Authenticator   *authenticator.Authenticator
-	AccountsService *accounts.Service
-	EMQStore        emq.Store
-	Tracer          opentracing.Tracer
-	TracerCloser    io.Closer
+type App struct {
+	Metrics       metrics.Metrics
+	Authenticator *authenticator.Authenticator
+	Tracer        opentracing.Tracer
+	TracerCloser  io.Closer
 }
 
+// nolint: gochecknoglobals
 var (
-	singleton *app
+	singleton *App
 	once      sync.Once
 )
 
-func GetInstance() *app {
+func GetInstance() *App {
 	once.Do(func() {
-		singleton = &app{}
+		singleton = new(App)
 	})
 
 	return singleton
 }
 
-func (a *app) SetEMQStore(store emq.Store) {
-	a.EMQStore = store
-}
-
-func (a *app) SetAuthenticator(authenticator *authenticator.Authenticator) {
+func (a *App) SetAuthenticator(authenticator *authenticator.Authenticator) {
 	a.Authenticator = authenticator
 }
 
-func (a *app) SetMetrics(m metrics.Metrics) {
+func (a *App) SetMetrics(m metrics.Metrics) {
 	a.Metrics = m
 }
 
-func (a *app) SetAccountsService(service *accounts.Service) {
-	a.AccountsService = service
-}
-
-func (a *app) SetTracer(tracer opentracing.Tracer, closer io.Closer) {
+func (a *App) SetTracer(tracer opentracing.Tracer, closer io.Closer) {
 	a.Tracer = tracer
 	a.TracerCloser = closer
 }
--- internal/authenticator/authenticator.go
+++ internal/authenticator/authenticator.go
@@ -1,11 +1,9 @@
 package authenticator
 
 import (
-	"context"
 	"crypto/rsa"
 	"errors"
 	"fmt"
-	"time"
 
 	"github.com/golang-jwt/jwt/v4"
 	"gitlab.snapp.ir/dispatching/snappids/v2"
@@ -27,50 +25,48 @@ var (
 	ErrIncorrectPassword    = errors.New("username or password is worng")
 )
 
-type ErrTopicNotAllowed struct {
+type TopicNotAllowedError struct {
 	Issuer     user.Issuer
 	Sub        string
 	AccessType acl.AccessType
 	Topic      topics.Topic
 }
 
-func (err ErrTopicNotAllowed) Error() string {
+func (err TopicNotAllowedError) Error() string {
 	return fmt.Sprintf("issuer %s with sub %s is not allowed to %s on topic %s (%s)",
 		err.Issuer, err.Sub, err.AccessType, err.Topic, err.Topic.GetType(),
 	)
 }
 
-type ErrPublicKeyNotFound struct {
+type PublicKeyNotFoundError struct {
 	Issuer user.Issuer
 }
 
-func (err ErrPublicKeyNotFound) Error() string {
+func (err PublicKeyNotFoundError) Error() string {
 	return fmt.Sprintf("cannot find issuer %s public key", err.Issuer)
 }
 
-type ErrInvalidTopic struct {
+type InvalidTopicError struct {
 	Topic topics.Topic
 }
 
-func (err ErrInvalidTopic) Error() string {
+func (err InvalidTopicError) Error() string {
 	return fmt.Sprintf("provided topic %s is not valid", err.Topic)
 }
 
 // Authenticator is responsible for Acl/Auth/Token of users.
 type Authenticator struct {
-	PrivateKeys            map[user.Issuer]*rsa.PrivateKey
-	PublicKeys             map[user.Issuer]*rsa.PublicKey
-	AllowedAccessTypes     []acl.AccessType
-	ModelHandler           db.ModelHandler
-	EMQTopicManager        *snappids.EMQTopicManager
-	HashIDSManager         *snappids.HashIDSManager
-	CompareHashAndPassword func([]byte, []byte) error
-	Company                string
+	PublicKeys         map[user.Issuer]*rsa.PublicKey
+	AllowedAccessTypes []acl.AccessType
+	ModelHandler       db.ModelHandler
+	EMQTopicManager    *snappids.EMQTopicManager
+	HashIDSManager     *snappids.HashIDSManager
+	Company            string
 }
 
 // Auth check user authentication by checking the user's token
 // isSuperuser is a flag that authenticator set it true when credentials is related to a superuser.
-func (a Authenticator) Auth(ctx context.Context, tokenString string) (isSuperuser bool, err error) {
+func (a Authenticator) Auth(tokenString string) (err error) {
 	_, err = jwt.Parse(tokenString, func(token *jwt.Token) (i interface{}, err error) {
 		if _, ok := token.Method.(*jwt.SigningMethodRSA); !ok {
 			return nil, ErrInvalidSigningMethod
@@ -84,29 +80,26 @@ func (a Authenticator) Auth(ctx context.Context, tokenString string) (isSuperuse
 			return nil, ErrIssNotFound
 		}
 
-		_, isSuperuser = claims["is_superuser"]
-
 		issuer := user.Issuer(fmt.Sprintf("%v", claims["iss"]))
 
 		key := a.PublicKeys[issuer]
 		if key == nil {
-			return nil, ErrPublicKeyNotFound{Issuer: issuer}
+			return nil, PublicKeyNotFoundError{Issuer: issuer}
 		}
 
 		return key, nil
 	})
-
 	if err != nil {
-		return false, fmt.Errorf("token is invalid: %w", err)
+		return fmt.Errorf("token is invalid: %w", err)
 	}
 
-	return isSuperuser, nil
+	return nil
 }
 
 // ACL check a user access to a topic.
-func (a Authenticator) ACL(ctx context.Context, accessType acl.AccessType,
+func (a Authenticator) ACL(accessType acl.AccessType,
 	tokenString string, topic topics.Topic) (bool, error) {
-	if !a.validateAccessType(accessType) {
+	if !a.ValidateAccessType(accessType) {
 		return false, ErrInvalidAccessType
 	}
 
@@ -129,7 +122,7 @@ func (a Authenticator) ACL(ctx context.Context, accessType acl.AccessType,
 		issuer := user.Issuer(fmt.Sprintf("%v", claims["iss"]))
 		key := a.PublicKeys[issuer]
 		if key == nil {
-			return nil, ErrPublicKeyNotFound{Issuer: issuer}
+			return nil, PublicKeyNotFoundError{Issuer: issuer}
 		}
 
 		return key, nil
@@ -157,136 +150,26 @@ func (a Authenticator) ACL(ctx context.Context, accessType acl.AccessType,
 
 	pk := primaryKey(issuer, sub)
 
-	var u user.User
-	if err := a.ModelHandler.Get(ctx, "user", pk, &u); err != nil {
-		return false, fmt.Errorf("error getting user %s from db err: %w", pk, err)
+	u := a.ModelHandler.Get(pk)
+
+	id, err := a.HashIDSManager.DecodeHashID(sub, issuerToAudience(issuer))
+	if err != nil {
+		return false, ErrDecodeHashID
 	}
 
-	// validate passenger and driver topics.
-	if issuer != user.ThirdParty {
-		id, err := a.HashIDSManager.DecodeHashID(sub, issuerToAudience(issuer))
-		if err != nil {
-			return false, ErrDecodeHashID
-		}
-
-		ok := a.ValidateTopicBySender(topic, issuerToAudience(issuer), id)
-		if !ok {
-			return false, ErrInvalidTopic{Topic: topic}
-		}
+	if !a.ValidateTopicBySender(topic, issuerToAudience(issuer), id) {
+		return false, InvalidTopicError{Topic: topic}
 	}
 
 	if ok := u.CheckTopicAllowance(topic.GetTypeWithCompany(a.Company), accessType); !ok {
 		return false,
-			ErrTopicNotAllowed{issuer, sub, accessType, topic}
+			TopicNotAllowedError{issuer, sub, accessType, topic}
 	}
 
 	return true, nil
 }
 
-// Token function issues JWT token by taking client credentials.
-func (a Authenticator) Token(ctx context.Context, _ acl.AccessType,
-	username, secret string) (string, error) {
-	var u user.User
-	if err := a.ModelHandler.Get(ctx, "user", username, &u); err != nil {
-		return "", fmt.Errorf("could not get user %s. %w", username, err)
-	}
-
-	if u.Secret != secret {
-		return "", ErrInvalidSecret
-	}
-
-	// nolint: exhaustivestruct
-	claims := jwt.StandardClaims{
-		ExpiresAt: time.Now().Add(u.TokenExpirationDuration).Unix(),
-		Issuer:    string(user.ThirdParty),
-		Subject:   username,
-	}
-
-	token := jwt.NewWithClaims(jwt.SigningMethodRS256, claims)
-
-	tokenString, err := token.SignedString(a.PrivateKeys[user.ThirdParty])
-	if err != nil {
-		return "", fmt.Errorf("could not sign the token %w", err)
-	}
-
-	return tokenString, nil
-}
-
-func (a Authenticator) HeraldToken(
-	username string,
-	endpoints []acl.Endpoint,
-	topics []acl.Topic,
-	duration time.Duration,
-) (string, error) {
-	// nolint: exhaustivestruct
-	claims := acl.Claims{
-		StandardClaims: jwt.StandardClaims{
-			ExpiresAt: time.Now().Add(duration).Unix(),
-			Issuer:    string(user.ThirdParty),
-			Subject:   username,
-		},
-		Topics:    topics,
-		Endpoints: endpoints,
-	}
-
-	token := jwt.NewWithClaims(jwt.SigningMethodRS256, claims)
-
-	tokenString, err := token.SignedString(a.PrivateKeys[user.ThirdParty])
-	if err != nil {
-		return "", fmt.Errorf("could not sign the token. %w", err)
-	}
-
-	return tokenString, nil
-}
-
-func (a Authenticator) SuperuserToken(username string, duration time.Duration) (string, error) {
-	// nolint: exhaustivestruct
-	claims := acl.SuperuserClaims{
-		StandardClaims: jwt.StandardClaims{
-			ExpiresAt: time.Now().Add(duration).Unix(),
-			Issuer:    string(user.ThirdParty),
-			Subject:   username,
-		},
-		IsSuperuser: true,
-	}
-
-	token := jwt.NewWithClaims(jwt.SigningMethodRS256, claims)
-
-	tokenString, err := token.SignedString(a.PrivateKeys[user.ThirdParty])
-	if err != nil {
-		return "", fmt.Errorf("could not sign the token. %w", err)
-	}
-
-	return tokenString, nil
-}
-
-func (a Authenticator) EndPointBasicAuth(ctx context.Context, username, password, endpoint string) (bool, error) {
-	var u user.User
-	if err := a.ModelHandler.Get(ctx, "user", username, &u); err != nil {
-		return false, fmt.Errorf("could not get user from db: %w", err)
-	}
-
-	if err := a.CompareHashAndPassword([]byte(u.Password), []byte(password)); err != nil {
-		return false, ErrIncorrectPassword
-	}
-
-	return u.CheckEndpointAllowance(endpoint), nil
-}
-
-func (a Authenticator) EndpointIPAuth(ctx context.Context, username string, ip string, endpoint string) (bool, error) {
-	var u user.User
-	if err := a.ModelHandler.Get(ctx, "user", username, &u); err != nil {
-		return false, fmt.Errorf("could not get user from db: %w", err)
-	}
-
-	if ok := acl.ValidateIP(ip, u.IPs, []string{}); !ok {
-		return false, ErrInvalidIP
-	}
-
-	return u.CheckEndpointAllowance(endpoint), nil
-}
-
-func (a Authenticator) validateAccessType(accessType acl.AccessType) bool {
+func (a Authenticator) ValidateAccessType(accessType acl.AccessType) bool {
 	for _, allowedAccessType := range a.AllowedAccessTypes {
 		if allowedAccessType == accessType {
 			return true
@@ -339,8 +222,6 @@ func issuerToAudience(issuer user.Issuer) snappids.Audience {
 		return snappids.PassengerAudience
 	case user.Driver:
 		return snappids.DriverAudience
-	case user.ThirdParty:
-		return snappids.ThirdPartyAudience
 	default:
 		return -1
 	}
