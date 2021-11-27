ec87832185e8d1b7ea8713d846c0914c15edf5e0
Parham Alvani <parham.alvani@gmail.com>
2021-11-27 16:15:26 +0330

    Revert "fix: correct deploy.sh file location"
    This reverts commit 4e8f5c97fb815584b6cbe9547fb73fa833bc373f.

--- deployments/soteria/deploy.sh
+++ deployments/soteria/deploy.sh
@@ -5,7 +5,7 @@ set -e
 echo "Rolling out soteria..."
 
 current_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-path_to_k8s="$current_dir/soteria"
+path_to_k8s="$current_dir"
 
 helm upgrade --install soteria "$path_to_k8s" \
 	-f "$path_to_k8s/values.yaml" \
