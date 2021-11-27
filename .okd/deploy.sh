6795f93b65061ef9ff485f2705bffe22a7414156
Parham Alvani <parham.alvani@gmail.com>
2021-11-27 12:50:41 +0330

    feat: add new ode files

--- .okd/deploy.sh
+++ .okd/deploy.sh
@@ -1,21 +1,11 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
-env > .env
+set -e
 
-echo "processing config map..."
-oc process -f ./mozart/ConfigMap.yaml --param-file=.env --ignore-unknown-parameters=true | oc apply -f -
+echo "Rolling out soteria..."
 
-echo "processing secret..."
-oc process -f ./mozart/Secret.yaml --param-file=.env --ignore-unknown-parameters=true | oc apply -f -
+current_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+path_to_k8s="$current_dir/../deployments/soteria"
 
-echo "processing service..."
-oc process -f ./mozart/Service.yaml --param-file=.env --ignore-unknown-parameters=true | oc apply -f -
-
-echo "processing deployment config..."
-oc process -f ./mozart/DeploymentConfig.yaml --param-file=.env --ignore-unknown-parameters=true | oc apply -f -
-
-echo "processing route..."
-oc process -f ./mozart/Route.yaml --param-file=.env --ignore-unknown-parameters=true | oc apply -f -
-
-echo "rolling out $SERVICE_NAME ..."
-oc rollout latest dc/$SERVICE_NAME
+helm upgrade --install soteria "$path_to_k8s" \
+	-f "$path_to_k8s/values.yaml"
