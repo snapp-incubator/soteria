f04f7d50a04fe2ed652e0729b1ee97a49306120c
Mehdi <mehditeymorian322@gmail.com>
2022-01-29 15:41:17 +0330

    feat: add topics templates to config

--- internal/config/default.go
+++ internal/config/default.go
@@ -122,5 +122,17 @@ func Default() Config {
 				},
 			},
 		},
+		Topics: map[string]string{
+			"cab_event":          `(\w+)-event-[a-zA-Z0-9]+`,
+			"driver_location":    `/driver/[a-zA-Z0-9]+/location`,
+			"passenger_location": `/passenger/[a-zA-Z0-9]+/location`,
+			"superapp_event":     `/(driver|passenger)/[a-zA-Z0-9]+/(superapp)`,
+			"box_event":          `bucks`,
+			"shared_location":    `/(driver|passenger)/[a-zA-Z0-9]+/(driver-location|passenger-location)`,
+			"chat":               `/(driver|passenger)/[a-zA-Z0-9]+/chat`,
+			"general_call_entry": `/(driver|passenger)/[a-zA-Z0-9]+/call/send`,
+			"node_call_entry":    `/(driver|passenger)/[a-zA-Z0-9]+/call/[a-zA-Z0-9-]+/send`,
+			"call_outgoing":      `/(driver|passenger)/[a-zA-Z0-9]+/call/receive`,
+		},
 	}
 }
