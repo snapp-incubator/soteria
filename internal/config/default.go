089292d29648a0d61a85d015fa2743faa1c82506
Mehdi <mohammadmehdi.teymorian@snapp.cab>
2022-01-30 09:55:16 +0330

    feat: add topics to config

--- internal/config/default.go
+++ internal/config/default.go
@@ -122,17 +122,57 @@ func Default() Config {
 				},
 			},
 		},
-		Topics: map[string]string{
-			"cab_event":          `(\w+)-event-[a-zA-Z0-9]+`,
-			"driver_location":    `/driver/[a-zA-Z0-9]+/location`,
-			"passenger_location": `/passenger/[a-zA-Z0-9]+/location`,
-			"superapp_event":     `/(driver|passenger)/[a-zA-Z0-9]+/(superapp)`,
-			"box_event":          `bucks`,
-			"shared_location":    `/(driver|passenger)/[a-zA-Z0-9]+/(driver-location|passenger-location)`,
-			"chat":               `/(driver|passenger)/[a-zA-Z0-9]+/chat`,
-			"general_call_entry": `/(driver|passenger)/[a-zA-Z0-9]+/call/send`,
-			"node_call_entry":    `/(driver|passenger)/[a-zA-Z0-9]+/call/[a-zA-Z0-9-]+/send`,
-			"call_outgoing":      `/(driver|passenger)/[a-zA-Z0-9]+/call/receive`,
+		Topics: []topics.Topic{
+			{
+				Type:     "cab_event",
+				Template: "{{.audience}}-event-{{.hashId}}",
+				Regex:    `(\w+)-event-[a-zA-Z0-9]+`,
+			},
+			{
+				Type:     "driver_location",
+				Template: "{{.company}}/driver/{{.hashId}}/location",
+				Regex:    `/driver/[a-zA-Z0-9]+/location`,
+			},
+			{
+				Type:     "passenger_location",
+				Template: "{{.company}}/passenger/{{.hashId}}/location",
+				Regex:    "/passenger/[a-zA-Z0-9]+/location",
+			},
+			{
+				Type:     "superapp_event",
+				Template: "{{.company}}/{{.audience}}/{{.hashId}}/superapp",
+				Regex:    `/(driver|passenger)/[a-zA-Z0-9]+/(superapp)`,
+			},
+			{
+				Type:     "box_event",
+				Template: "bucks",
+				Regex:    "bucks",
+			},
+			{
+				Type:     "shared_location",
+				Template: "{{.company}}/{{.audience}}/{{.hashId}}/{{.peer}}-location",
+				Regex:    `/(driver|passenger)/[a-zA-Z0-9]+/(driver-location|passenger-location)`,
+			},
+			{
+				Type:     "chat",
+				Template: "{{.company}}/{{.audience}}/{{.hashId}}/chat",
+				Regex:    `/(driver|passenger)/[a-zA-Z0-9]+/chat`,
+			},
+			{
+				Type:     "general_call_entry",
+				Template: "shared/{{.company}}/{{.audience}}/{{.hashId}}/call/send",
+				Regex:    `/(driver|passenger)/[a-zA-Z0-9]+/call/send`,
+			},
+			{
+				Type:     "node_call_entry",
+				Template: "{{.company}}/{{.audience}}/{{.hashId}}/call/{{.node}}/send",
+				Regex:    `/(driver|passenger)/[a-zA-Z0-9]+/call/[a-zA-Z0-9-]+/send`,
+			},
+			{
+				Type:     "call_outgoing",
+				Template: "{{.company}}/{{.audience}}/{{.hashId}}/call/receive",
+				Regex:    `/(driver|passenger)/[a-zA-Z0-9]+/call/receive`,
+			},
 		},
 	}
 }
