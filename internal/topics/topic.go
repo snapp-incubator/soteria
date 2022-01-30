cdcba63a3617245f9983e69de6406f8246325b4c
Mehdi <mohammadmehdi.teymorian@snapp.cab>
2022-01-30 09:54:47 +0330

    feat: add topic and topic template

---
+++ internal/topics/topic.go
@@ -0,0 +1,28 @@
+package topics
+
+import (
+	"regexp"
+	"strings"
+	"text/template"
+)
+
+type Topic struct {
+	Type     string `koanf:"type"`
+	Template string `koanf:"template"`
+	Regex    string `koanf:"regex"`
+}
+
+type Template struct {
+	Type     string
+	Template *template.Template
+	Regex    *regexp.Regexp
+}
+
+func (t Template) Parse(fields map[string]string) string {
+	writer := new(strings.Builder)
+	err := t.Template.Execute(writer, fields)
+	if err != nil {
+		return ""
+	}
+	return writer.String()
+}
