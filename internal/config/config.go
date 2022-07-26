1a30b07ca686c269c0ba07f4f62623ee5084aced
Mohammadmehdi Teymorian <mohammadmehdi.teymorian@snapp.cab>
2022-07-26 10:04:11 +0430

    feat: move functions to main

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,15 +1,10 @@
 package config
 
 import (
-	"crypto/rsa"
 	"encoding/json"
-	"errors"
-	"fmt"
-	"io/ioutil"
 	"log"
 	"strings"
 
-	"github.com/golang-jwt/jwt/v4"
 	"github.com/knadh/koanf"
 	"github.com/knadh/koanf/parsers/yaml"
 	"github.com/knadh/koanf/providers/env"
@@ -19,8 +14,6 @@ import (
 	"gitlab.snapp.ir/dispatching/soteria/internal/logger"
 	"gitlab.snapp.ir/dispatching/soteria/internal/topics"
 	"gitlab.snapp.ir/dispatching/soteria/internal/tracing"
-	"gitlab.snapp.ir/dispatching/soteria/pkg/acl"
-	"gitlab.snapp.ir/dispatching/soteria/pkg/user"
 )
 
 const (
@@ -96,62 +89,4 @@ func New() Config {
 	log.Printf(tmpl, string(indent))
 
 	return instance
-}
-
-// ReadPrivateKey will read and return private key that is used for JWT encryption.
-// nolint: wrapcheck, goerr113
-func (a *Config) ReadPublicKey(u user.Issuer) (*rsa.PublicKey, error) {
-	var fileName string
-
-	switch u { // nolint:exhaustive
-	case user.Driver:
-		fileName = fmt.Sprintf("%s%s", a.JWT.Path, "0.pem")
-	case user.Passenger:
-		fileName = fmt.Sprintf("%s%s", a.JWT.Path, "1.pem")
-	default:
-		return nil, errors.New("invalid issuer, public key not found")
-	}
-
-	pem, err := ioutil.ReadFile(fileName)
-	if err != nil {
-		return nil, err
-	}
-
-	publicKey, err := jwt.ParseRSAPublicKeyFromPEM(pem)
-	if err != nil {
-		return nil, err
-	}
-
-	return publicKey, nil
-}
-
-// GetAllowedAccessTypes will return all allowed access types in Soteria.
-func (a *Config) GetAllowedAccessTypes() ([]acl.AccessType, error) {
-	allowedAccessTypes := make([]acl.AccessType, 0, len(a.AllowedAccessTypes))
-
-	for _, a := range a.AllowedAccessTypes {
-		at, err := toUserAccessType(a)
-		if err != nil {
-			return nil, fmt.Errorf("could not convert %s: %w", at, err)
-		}
-
-		allowedAccessTypes = append(allowedAccessTypes, at)
-	}
-
-	return allowedAccessTypes, nil
-}
-
-// toUserAccessType will convert string access type to it's own type.
-// nolint: goerr113
-func toUserAccessType(access string) (acl.AccessType, error) {
-	switch access {
-	case "pub":
-		return acl.Pub, nil
-	case "sub":
-		return acl.Sub, nil
-	case "pubsub":
-		return acl.PubSub, nil
-	}
-
-	return "", fmt.Errorf("%v is a invalid acces type", access)
 }
