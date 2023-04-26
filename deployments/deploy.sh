52c64d84f6981a3599e4a9051d9dbef4c19cb618
Parham Alvani <parham.alvani@gmail.com>
2023-04-26 13:17:51 +0330

    feat: update deploy script to support baly

--- deployments/deploy.sh
+++ deployments/deploy.sh
@@ -2,11 +2,14 @@
 
 set -e
 
-echo "Rolling out soteria..."
+company="$(oc project -q | cut -d- -f1)"
+ode="$(oc project -q | cut -d- -f3)"
+
+echo "Rolling out soteria on $company-ode-$ode"
 
 current_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 path_to_k8s="$current_dir/soteria"
 
 helm upgrade --install soteria "$path_to_k8s" \
 	-f "$path_to_k8s/values.yaml" \
-	-f "$path_to_k8s/values.ode.yaml"
+	-f "$path_to_k8s/values.ode.$company.yaml"
