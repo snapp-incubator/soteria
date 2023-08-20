2ab9c5364bb4659488bdc098786c4ade0e45907d
Parham Alvani <parham.alvani@gmail.com>
2023-08-20 03:00:09 -0400

    chore: downgrade jwt package

--- internal/authenticator/auto_authenticator.go
+++ internal/authenticator/auto_authenticator.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/golang-jwt/jwt/v5"
+	"github.com/golang-jwt/jwt/v4"
 	"gitlab.snapp.ir/dispatching/soteria/internal/config"
 	"gitlab.snapp.ir/dispatching/soteria/internal/topics"
 	"gitlab.snapp.ir/dispatching/soteria/pkg/acl"
--- internal/authenticator/manual_authenticator_test.go
+++ internal/authenticator/manual_authenticator_test.go
@@ -7,7 +7,7 @@ import (
 	"testing"
 	"time"
 
-	"github.com/golang-jwt/jwt/v5"
+	"github.com/golang-jwt/jwt/v4"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/suite"
 	"gitlab.snapp.ir/dispatching/soteria/internal/authenticator"
