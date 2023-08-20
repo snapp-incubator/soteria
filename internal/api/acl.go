a9735d127315590ae05ce71750abc8e1d68d5dc7
Parham Alvani <parham.alvani@gmail.com>
2023-08-20 00:17:35 -0400

    feat: update again to v5 but using a shared parser

--- internal/api/acl.go
+++ internal/api/acl.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 
 	"github.com/gofiber/fiber/v2"
-	"github.com/golang-jwt/jwt/v4"
+	"github.com/golang-jwt/jwt/v5"
 	"gitlab.snapp.ir/dispatching/soteria/internal/authenticator"
 	"gitlab.snapp.ir/dispatching/soteria/pkg/acl"
 	"go.opentelemetry.io/otel/attribute"
--- internal/authenticator/builder.go
+++ internal/authenticator/builder.go
@@ -3,7 +3,7 @@ package authenticator
 import (
 	"fmt"
 
-	"github.com/golang-jwt/jwt/v4"
+	"github.com/golang-jwt/jwt/v5"
 	"gitlab.snapp.ir/dispatching/soteria/internal/config"
 	"gitlab.snapp.ir/dispatching/soteria/internal/topics"
 	"gitlab.snapp.ir/dispatching/soteria/pkg/acl"
