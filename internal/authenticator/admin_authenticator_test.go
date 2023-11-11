152970d4c3b18183371b2fdaf80b190f41b2d7d0
Parham Alvani <parham.alvani@gmail.com>
2023-11-11 23:33:26 +0000

    feat: improve test coverage

---
+++ internal/authenticator/admin_authenticator_test.go
@@ -0,0 +1,65 @@
+package authenticator_test
+
+import (
+	"testing"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/snapp-incubator/soteria/internal/authenticator"
+	"github.com/snapp-incubator/soteria/internal/config"
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+)
+
+type AdminAuthenticatorTestSuite struct {
+	suite.Suite
+
+	AdminToken string
+
+	Authenticator authenticator.Authenticator
+}
+
+func TestAdminAuthenticator_suite(t *testing.T) {
+	t.Parallel()
+
+	st := new(AdminAuthenticatorTestSuite)
+
+	pkey0, err := getPublicKey("admin")
+	require.NoError(t, err)
+
+	st.Authenticator = authenticator.AdminAuthenticator{
+		Key:     pkey0,
+		Company: "snapp-admin",
+		Parser:  jwt.NewParser(),
+		JwtConfig: config.Jwt{
+			IssName:       "iss",
+			SubName:       "sub",
+			SigningMethod: "rsa256",
+		},
+	}
+
+	suite.Run(t, st)
+}
+
+func (suite *AdminAuthenticatorTestSuite) SetupSuite() {
+	require := suite.Require()
+
+	key, err := getPrivateKey("admin")
+	require.NoError(err)
+
+	adminToken, err := getSampleToken("admin", key)
+	require.NoError(err)
+
+	suite.AdminToken = adminToken
+}
+
+func (suite *AdminAuthenticatorTestSuite) TestAuth() {
+	require := suite.Require()
+
+	suite.Run("testing admin token auth", func() {
+		require.NoError(suite.Authenticator.Auth(suite.AdminToken))
+	})
+
+	suite.Run("testing invalid token auth", func() {
+		require.Error(suite.Authenticator.Auth(invalidToken))
+	})
+}
