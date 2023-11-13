5ee540e83489f4847cc1b3bb16fde3ab89ad344b
Parham Alvani <parham.alvani@gmail.com>
2023-11-13 04:22:29 +0000

    feat: update tracer usage

--- internal/tracing/tracer.go
+++ internal/tracing/tracer.go
@@ -10,12 +10,13 @@ import (
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
 	semconv "go.opentelemetry.io/otel/semconv/v1.7.0"
 	"go.opentelemetry.io/otel/trace"
+	"go.opentelemetry.io/otel/trace/noop"
 	"go.uber.org/zap"
 )
 
-func New(cfg Config, logger *zap.Logger) trace.Tracer { //nolint: ireturn
+func New(cfg Config, logger *zap.Logger) trace.Tracer {
 	if !cfg.Enabled {
-		return trace.NewNoopTracerProvider().Tracer("snapp.dispatching")
+		return noop.NewTracerProvider().Tracer("snapp.dispatching")
 	}
 
 	exporter, err := otlptracegrpc.New(
