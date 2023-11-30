32faeae8629ea59f37064a9b8f8193210b988daf
Parham Alvani <parham.alvani@gmail.com>
2023-11-30 20:35:26 +0000

    feat: increase test coverage

--- internal/authenticator/builder_test.go
+++ internal/authenticator/builder_test.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/snapp-incubator/soteria/internal/authenticator"
 	"github.com/snapp-incubator/soteria/internal/config"
+	"github.com/snapp-incubator/soteria/internal/topics"
 	"github.com/stretchr/testify/require"
 	"go.uber.org/zap"
 )
@@ -101,4 +102,58 @@ func TestBuilderInternalAuthenticatorWithInvalidKey(t *testing.T) {
 
 	_, err := b.Authenticators()
 	require.ErrorIs(err, authenticator.ErrAdminAuthenticatorSystemKey)
+}
+
+func TestBuilderManualAuthenticatorWithoutKey(t *testing.T) {
+	t.Parallel()
+
+	require := require.New(t)
+
+	b := authenticator.Builder{
+		Vendors: []config.Vendor{
+			{
+				Company: "internal",
+				Jwt: config.Jwt{
+					IssName:       "iss",
+					SubName:       "sub",
+					SigningMethod: "HS512",
+				},
+				IsInternal:         false,
+				UseValidator:       false,
+				AllowedAccessTypes: []string{"pub", "sub"},
+				Topics:             nil,
+				HashIDMap: map[string]topics.HashData{
+					"0": {
+						Alphabet: "",
+						Length:   15,
+						Salt:     "secret",
+					},
+					"1": {
+						Alphabet: "",
+						Length:   15,
+						Salt:     "secret",
+					},
+				},
+				IssEntityMap: map[string]string{
+					"0":       "driver",
+					"1":       "passenger",
+					"default": "",
+				},
+				IssPeerMap: map[string]string{
+					"0":       "passenger",
+					"1":       "driver",
+					"default": "",
+				},
+				Keys: nil,
+			},
+		},
+		Logger: zap.NewNop(),
+		ValidatorConfig: config.Validator{
+			URL:     "",
+			Timeout: 0,
+		},
+	}
+
+	_, err := b.Authenticators()
+	require.ErrorIs(err, authenticator.ErrNoKeys)
 }
