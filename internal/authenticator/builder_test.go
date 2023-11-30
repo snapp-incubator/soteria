526a9fc448d6ed648b4640a4ba00f23d8bc9e773
Parham Alvani <parham.alvani@gmail.com>
2023-11-30 20:02:09 +0000

    feat: add system key not found error

--- internal/authenticator/builder_test.go
+++ internal/authenticator/builder_test.go
@@ -47,3 +47,40 @@ func TestBuilderInternalAuthenticator(t *testing.T) {
 	require.Len(vendors, 1)
 	require.Contains(vendors, "internal")
 }
+
+func TestBuilderInternalAuthenticatorWithInvalidKey(t *testing.T) {
+	t.Parallel()
+
+	require := require.New(t)
+
+	b := authenticator.Builder{
+		Vendors: []config.Vendor{
+			{
+				Company: "internal",
+				Jwt: config.Jwt{
+					IssName:       "iss",
+					SubName:       "sub",
+					SigningMethod: "HS512",
+				},
+				IsInternal:         true,
+				UseValidator:       false,
+				AllowedAccessTypes: nil,
+				Topics:             nil,
+				HashIDMap:          nil,
+				IssEntityMap:       nil,
+				IssPeerMap:         nil,
+				Keys: map[string]string{
+					"not-system": "c2VjcmV0",
+				},
+			},
+		},
+		Logger: zap.NewNop(),
+		ValidatorConfig: config.Validator{
+			URL:     "",
+			Timeout: 0,
+		},
+	}
+
+	_, err := b.Authenticators()
+	require.ErrorIs(err, authenticator.ErrAdminAuthenticatorSystemKey)
+}
