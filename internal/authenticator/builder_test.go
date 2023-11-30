4b21b0cd34a27d73c6b2f9eb4def354fad5c1cec
Parham Alvani <parham.alvani@gmail.com>
2023-11-30 19:59:02 +0000

    feat: improve the builder pattern and increase coverage

---
+++ internal/authenticator/builder_test.go
@@ -0,0 +1,49 @@
+package authenticator_test
+
+import (
+	"testing"
+
+	"github.com/snapp-incubator/soteria/internal/authenticator"
+	"github.com/snapp-incubator/soteria/internal/config"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/zap"
+)
+
+func TestBuilderInternalAuthenticator(t *testing.T) {
+	t.Parallel()
+
+	require := require.New(t)
+
+	b := authenticator.Builder{
+		Vendors: []config.Vendor{
+			{
+				Company: "internal",
+				Jwt: config.Jwt{
+					IssName:       "iss",
+					SubName:       "sub",
+					SigningMethod: "HS512",
+				},
+				IsInternal:         true,
+				UseValidator:       false,
+				AllowedAccessTypes: nil,
+				Topics:             nil,
+				HashIDMap:          nil,
+				IssEntityMap:       nil,
+				IssPeerMap:         nil,
+				Keys: map[string]string{
+					"system": "c2VjcmV0",
+				},
+			},
+		},
+		Logger: zap.NewNop(),
+		ValidatorConfig: config.Validator{
+			URL:     "",
+			Timeout: 0,
+		},
+	}
+
+	vendors, err := b.Authenticators()
+	require.NoError(err)
+	require.Len(vendors, 1)
+	require.Contains(vendors, "internal")
+}
