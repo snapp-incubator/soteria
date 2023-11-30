91679cd61dc6bc1c229dc044e0f8abdb2edc95cd
Parham Alvani <parham.alvani@gmail.com>
2023-11-30 20:29:48 +0000

    feat: handle errors for loading keys

--- internal/authenticator/key.go
+++ internal/authenticator/key.go
@@ -2,30 +2,50 @@ package authenticator
 
 import (
 	"encoding/base64"
+	"errors"
+	"fmt"
 	"strings"
 
 	"github.com/golang-jwt/jwt/v5"
 	"go.uber.org/zap"
 )
 
-func (b Builder) GenerateKeys(method string, keys map[string]string) map[string]any {
-	var keyList map[string]any
+var (
+	ErrInvalidKeyType = errors.New("cannot determine the key type")
+	ErrNoKeys         = errors.New("at least one key required")
+)
 
-	// ES RS HS PS EdDSA
+func (b Builder) GenerateKeys(method string, keys map[string]string) (map[string]any, error) {
+	var (
+		keyList map[string]any
+		err     error
+	)
+
+	// https://jwt.io/
 	switch {
 	case strings.HasPrefix(method, "RS"):
-		keyList = b.GenerateRsaKeys(keys)
+		keyList, err = b.GenerateRSAKeys(keys)
 	case strings.HasPrefix(method, "HS"):
-		keyList = b.GenerateHMacKeys(keys)
+		keyList, err = b.GenerateHMACKeys(keys)
+	case strings.HasPrefix(method, "ES"):
+		keyList, err = b.GenerateECDSAKeys(keys)
 	default:
-		keyList = make(map[string]any)
+		return nil, ErrInvalidKeyType
 	}
 
-	return keyList
+	if err != nil {
+		return nil, fmt.Errorf("reading keys failed %w", err)
+	}
+
+	if len(keyList) == 0 {
+		return nil, ErrNoKeys
+	}
+
+	return keyList, nil
 }
 
-func (b Builder) GenerateRsaKeys(raw map[string]string) map[string]any {
-	rsaKeys := make(map[string]any)
+func (b Builder) GenerateRSAKeys(raw map[string]string) (map[string]any, error) {
+	keys := make(map[string]any)
 
 	for iss, publicKey := range raw {
 		bytes, err := jwt.ParseRSAPublicKeyFromPEM([]byte(publicKey))
@@ -33,23 +53,38 @@ func (b Builder) GenerateRsaKeys(raw map[string]string) map[string]any {
 			b.Logger.Fatal("could not read public key", zap.String("issuer", iss), zap.Error(err))
 		}
 
-		rsaKeys[iss] = bytes
+		keys[iss] = bytes
 	}
 
-	return rsaKeys
+	return keys, nil
 }
 
-func (b Builder) GenerateHMacKeys(raw map[string]string) map[string]any {
+func (b Builder) GenerateECDSAKeys(raw map[string]string) (map[string]any, error) {
 	keys := make(map[string]any)
 
-	for iss, key := range raw {
-		bytes, err := base64.StdEncoding.DecodeString(key)
+	for iss, publicKey := range raw {
+		bytes, err := jwt.ParseECPublicKeyFromPEM([]byte(publicKey))
 		if err != nil {
-			b.Logger.Fatal("failed to generate hmac key from base64", zap.Error(err))
+			b.Logger.Fatal("could not read public key", zap.String("issuer", iss), zap.Error(err))
 		}
 
 		keys[iss] = bytes
 	}
 
-	return keys
+	return keys, nil
+}
+
+func (b Builder) GenerateHMACKeys(raw map[string]string) (map[string]any, error) {
+	keys := make(map[string]any)
+
+	for iss, key := range raw {
+		bytes, err := base64.StdEncoding.DecodeString(key)
+		if err != nil {
+			return nil, fmt.Errorf("failed to generate hmac key from base64 %w", err)
+		}
+
+		keys[iss] = bytes
+	}
+
+	return keys, nil
 }
