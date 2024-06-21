7693ebd90e71b9ef6a6406bae7809aa81d898b31
Ahmad Anvari <ahmad.anv@gmail.com>
2024-06-21 19:37:22 +0330

    fix: lint issues

--- internal/authenticator/metrics.go
+++ internal/authenticator/metrics.go
@@ -7,6 +7,7 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promauto"
 )
 
+// nolint:exhaustruct,gochecknoglobals
 var AuthenticateCounterMetric = promauto.NewCounterVec(prometheus.CounterOpts{
 	Namespace: "dispatching",
 	Subsystem: "soteria",
@@ -14,36 +15,40 @@ var AuthenticateCounterMetric = promauto.NewCounterVec(prometheus.CounterOpts{
 	Help:      "Total number of authentication attempts",
 }, []string{"company", "status"})
 
+// nolint:cyclop
 func IncrementWithErrorAuthCounter(company string, err error) {
-	var status string
-	var topicNotAllowedErrorTarget *TopicNotAllowedError
-	var keyNotFoundErrorTarget *KeyNotFoundError
+	var (
+		status                     string
+		topicNotAllowedErrorTarget *TopicNotAllowedError
+		keyNotFoundErrorTarget     *KeyNotFoundError
+	)
 
-	if err != nil {
+	switch {
+	case err == nil:
 		status = "success"
-	} else if errors.Is(err, ErrInvalidSigningMethod) {
+	case errors.Is(err, ErrInvalidSigningMethod):
 		status = "err_invalid_signing_method"
-	} else if errors.Is(err, ErrIssNotFound) {
+	case errors.Is(err, ErrIssNotFound):
 		status = "err_iss_not_found"
-	} else if errors.Is(err, ErrSubNotFound) {
+	case errors.Is(err, ErrSubNotFound):
 		status = "err_sub_not_found"
-	} else if errors.Is(err, ErrInvalidClaims) {
+	case errors.Is(err, ErrInvalidClaims):
 		status = "err_invalid_claims"
-	} else if errors.Is(err, ErrInvalidIP) {
+	case errors.Is(err, ErrInvalidIP):
 		status = "err_invalid_ip"
-	} else if errors.Is(err, ErrInvalidAccessType) {
+	case errors.Is(err, ErrInvalidAccessType):
 		status = "err_invalid_access_type"
-	} else if errors.Is(err, ErrDecodeHashID) {
+	case errors.Is(err, ErrDecodeHashID):
 		status = "err_decode_hash_id"
-	} else if errors.Is(err, ErrInvalidSecret) {
+	case errors.Is(err, ErrInvalidSecret):
 		status = "err_invalid_secret"
-	} else if errors.Is(err, ErrIncorrectPassword) {
+	case errors.Is(err, ErrIncorrectPassword):
 		status = "err_incorrect_password"
-	} else if errors.As(err, &topicNotAllowedErrorTarget) {
+	case errors.As(err, &topicNotAllowedErrorTarget):
 		status = "topic_not_allowed_error"
-	} else if errors.As(err, &keyNotFoundErrorTarget) {
+	case errors.As(err, &keyNotFoundErrorTarget):
 		status = "key_not_found_error"
-	} else {
+	default:
 		status = "unknown_error"
 	}
 
