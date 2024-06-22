3a86f3ccc0fb3c418bfe4353279fbca317ae8a3f
Ahmad Anvari <ahmad.anv@gmail.com>
2024-06-22 19:17:54 +0330

    fix: lint issue

--- internal/authenticator/metrics_test.go
+++ internal/authenticator/metrics_test.go
@@ -1,9 +1,11 @@
+//nolint:testpackage
 package authenticator
 
 import (
 	"testing"
 )
 
+//nolint:paralleltest
 func TestIncrementWithErrorAuthCounter(_ *testing.T) {
 	IncrementWithErrorAuthCounter("snapp", ErrIssNotFound)
 }
