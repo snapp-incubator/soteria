a3a1514d19d4850262c45393922a136204b1ab77
Parham Alvani <parham.alvani@gmail.com>
2024-09-10 18:11:11 +0000

    fix: correct converting sub to string

--- internal/authenticator/auto_authenticator.go
+++ internal/authenticator/auto_authenticator.go
@@ -77,7 +77,7 @@ func (a AutoAuthenticator) ACL(
 		return false, ErrSubNotFound
 	}
 
-	sub, _ := claims[a.JWTConfig.SubName].(string)
+	sub := fmt.Sprintf("%v", claims[a.JWTConfig.SubName])
 
 	topicTemplate := a.TopicManager.ParseTopic(topic, issuer, sub, map[string]any(claims))
 	if topicTemplate == nil {
