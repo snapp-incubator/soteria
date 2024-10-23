c6ef68391f758757efc523ebf8b8cb2c2aeb61de
Parham Alvani <parham.alvani@gmail.com>
2024-10-23 11:13:50 +0000

    feat: add another client-id test

--- internal/clientid/clientid_test.go
+++ internal/clientid/clientid_test.go
@@ -7,14 +7,14 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestParser(t *testing.T) {
+func TestParser1(t *testing.T) {
 	t.Parallel()
 
 	require := require.New(t)
 
 	p := clientid.NewParser(clientid.Config{
 		Patterns: map[string]string{
-			"android": "^android_\\d+$",
+			"android": `^android_\d+$`,
 		},
 	})
 
@@ -22,4 +22,18 @@ func TestParser(t *testing.T) {
 	require.Equal("-", p.Parse("android_124_"))
 	require.Equal("-", p.Parse("hello"))
 	require.Equal("-", p.Parse("_android_124"))
+}
+
+func TestParser2(t *testing.T) {
+	t.Parallel()
+
+	require := require.New(t)
+
+	p := clientid.NewParser(clientid.Config{
+		Patterns: map[string]string{
+			"android_driver": `^AD#([a-f\d]{1,32}|[A-F\d]{1,32})#\d+$`,
+		},
+	})
+
+	require.Equal("android_driver", p.Parse("AD#e7adbbd4022e6ae762aae11b0593236#79971153921523"))
 }
